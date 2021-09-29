486ba7b4cd3799d97b89d9f6016bfe1f104fc3f4
chriad <chriad.mueller@protonmail.com>
2021-09-29 14:40:00 +0200

    Whole page!

--- archivperlen/spiders/archivperlen_spider.py
+++ archivperlen/spiders/archivperlen_spider.py
@@ -5,14 +5,11 @@ from scrapy.exporters import CsvItemExporter
 
 
 class Archivperle(Item):
-    vis_desc = Field()
     url = Field()
-    vis_desc = Field()
-    vis_desc = Field()
-    vis_desc = Field()
-    vis_desc = Field()
-    vis_desc = Field()
-    vis_desc = Field()
+    title = Field()
+    tooltip = Field()
+    description = Field()
+
 
 
 class ArchivperlenSpider(scrapy.Spider):
@@ -25,25 +22,30 @@ class ArchivperlenSpider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        item = response.css("ul li:first-of-type article div:first-of-type")
-        anchor = item.css("a")[1]
+        # item = response.css("ul li:first-of-type article div:first-of-type")
+        articles = response.css("article")
 
-        dl_url = anchor.attrib["href"]
-        description = anchor.attrib["aria-label"]
-        title = item.css("a:nth-of-type(2)::text").get()  # other way
-        desc_long = item.css("div div:first-of-type::text").get()
-
-        metadata = item.css("div div:nth-of-type(2)")
-        published = metadata.css("time::text").get()
-        duration = metadata.css("span:nth-of-type(2)::text").get()
-
-        self.log(
-            f'\n{dl_url}\n{description}\n{title}\n{desc_long}\n{published}\n{duration}')
-        perle = Archivperle(vis_desc=title, url=dl_url)
+        
         filename = f'/home/chriad/Desktop/archivperlen/data.csv'
         with open(filename, 'wb') as f:
             exporter = CsvItemExporter(file=f)
             exporter.start_exporting()
-            exporter.export_item(perle)
+            for article in articles:
+                item = article.css("div:first-of-type")
+                anchor = item.css("a")[1]
+
+                dl_url = anchor.attrib["href"]
+                tooltip = anchor.attrib["aria-label"]
+                title = item.css("a:nth-of-type(2)::text").get()  # other way
+                description = item.css("div div:first-of-type::text").get()
+
+                metadata = item.css("div div:nth-of-type(2)")
+                published = metadata.css("time::text").get()
+                duration = metadata.css("span:nth-of-type(2)::text").get()
+
+                # self.log(
+                #     f'\n{dl_url}\n{tooltip}\n{title}\n{description}\n{published}\n{duration}')
+
+                perle = Archivperle(title=title, url=dl_url, tooltip=tooltip, description=description)
+                exporter.export_item(perle)
             exporter.finish_exporting()
