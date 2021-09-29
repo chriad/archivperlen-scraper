6849f7e573f746a855d0c994b88f8db0ad9b490c
chriad <chriad.mueller@protonmail.com>
2021-09-29 14:24:26 +0200

    renamed

--- archivperlen/spiders/archivperlen_spider.py
+++ archivperlen/spiders/archivperlen_spider.py
@@ -3,43 +3,47 @@ from scrapy.item import Item, Field
 from itemadapter import ItemAdapter
 from scrapy.exporters import CsvItemExporter
 
+
 class Archivperle(Item):
     vis_desc = Field()
     url = Field()
+    vis_desc = Field()
+    vis_desc = Field()
+    vis_desc = Field()
+    vis_desc = Field()
+    vis_desc = Field()
+    vis_desc = Field()
+
 
 class ArchivperlenSpider(scrapy.Spider):
     name = "archivperlen"
     base_url = 'https://www.srf.ch'
-
 
     def start_requests(self):
 
         url = 'https://www.srf.ch/play/suche?query=&showAll=true&hideShowSearch=true&shows=urn%3Asrf%3Ashow%3Atv%3Aeb4f3b13-0362-4ea8-b7b3-325382c86ef2'
         yield scrapy.Request(url=url, callback=self.parse)
 
-        
-
-        
-
     def parse(self, response):
         # page = response.url.split("/")[-2]
         item = response.css("ul li:first-of-type article div:first-of-type")
         anchor = item.css("a")[1]
+
         dl_url = anchor.attrib["href"]
-        context_desc = anchor.attrib["aria-label"]
-        vis_desc = item.css("a:nth-of-type(2)::text").get() # other way
+        description = anchor.attrib["aria-label"]
+        title = item.css("a:nth-of-type(2)::text").get()  # other way
         desc_long = item.css("div div:first-of-type::text").get()
+
         metadata = item.css("div div:nth-of-type(2)")
-        datep = metadata.css("time::text").get()
+        published = metadata.css("time::text").get()
         duration = metadata.css("span:nth-of-type(2)::text").get()
-   
-        self.log(f'Saved file {dl_url}\n{context_desc}\n{vis_desc}\n{datep}\n')
-        perle = Archivperle(vis_desc=vis_desc, url=dl_url)
+
+        self.log(
+            f'\n{dl_url}\n{description}\n{title}\n{desc_long}\n{published}\n{duration}')
+        perle = Archivperle(vis_desc=title, url=dl_url)
         filename = f'/home/chriad/Desktop/archivperlen/data.csv'
         with open(filename, 'wb') as f:
             exporter = CsvItemExporter(file=f)
             exporter.start_exporting()
             exporter.export_item(perle)
             exporter.finish_exporting()
-        
-
