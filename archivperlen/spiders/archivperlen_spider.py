85b0a9f6a74ca93cbf1428bcce6da5991d91ae36
chriad <chriad.mueller@protonmail.com>
2021-09-29 13:54:26 +0200

    With csv export

--- archivperlen/spiders/archivperlen_spider.py
+++ archivperlen/spiders/archivperlen_spider.py
@@ -1,16 +1,25 @@
 import scrapy
+from scrapy.item import Item, Field
+from itemadapter import ItemAdapter
+from scrapy.exporters import CsvItemExporter
 
+class Archivperle(Item):
+    vis_desc = Field()
+    url = Field()
 
 class ArchivperlenSpider(scrapy.Spider):
     name = "archivperlen"
     base_url = 'https://www.srf.ch'
 
+
     def start_requests(self):
-        urls = [
-            'https://www.srf.ch/play/suche?query=&showAll=true&hideShowSearch=true&shows=urn%3Asrf%3Ashow%3Atv%3Aeb4f3b13-0362-4ea8-b7b3-325382c86ef2'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+
+        url = 'https://www.srf.ch/play/suche?query=&showAll=true&hideShowSearch=true&shows=urn%3Asrf%3Ashow%3Atv%3Aeb4f3b13-0362-4ea8-b7b3-325382c86ef2'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+        
+
+        
 
     def parse(self, response):
         # page = response.url.split("/")[-2]
@@ -23,7 +32,14 @@ class ArchivperlenSpider(scrapy.Spider):
         metadata = item.css("div div:nth-of-type(2)")
         datep = metadata.css("time::text").get()
         duration = metadata.css("span:nth-of-type(2)::text").get()
-        # filename = f'quotes-{page}.html'
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        self.log(f'Saved file {dl_url}')
+   
+        self.log(f'Saved file {dl_url}\n{context_desc}\n{vis_desc}\n{datep}\n')
+        perle = Archivperle(vis_desc=vis_desc, url=dl_url)
+        filename = f'/home/chriad/Desktop/archivperlen/data.csv'
+        with open(filename, 'wb') as f:
+            exporter = CsvItemExporter(file=f)
+            exporter.start_exporting()
+            exporter.export_item(perle)
+            exporter.finish_exporting()
+        
+
